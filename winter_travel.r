6b992b50739377b323d7c4a45ee3841357af747c
Zack Treisman <ztreisman@gmail.com>
2021-01-11 08:36:37 -0700

    Clean capitalization and Slate River (Rd)

--- winter_travel.r
+++ winter_travel.r
@@ -3,7 +3,7 @@
 ## Winter Travel Data Collection Initiative
 ## Zack Treisman
 ## 12/7/20
-## modified 12/21/20
+## modified 1/11/21
 ##
 #############
 
@@ -17,7 +17,7 @@ library(gam)
 
 # Load trailhead data
 
-# Changes to spreadsheets: remove spaces from directory names, Zach -> Zack,  convert to csv, simplify names, remove summaries at bottom of some files
+# Changes to spreadsheets: remove spaces from directory names, Zach -> Zack,  convert to csv, simplify names, remove summaries and other items at bottom of some files
 # Use read.csv instead of read_csv because Non-motorized has that dash and read_csv makes that a quoted string instead of converting it to a dot. Probably not worth the trouble to fix in the spreadsheets.
 
 # Snow bike to motorized: for 17-18 there were no changes for Brush Creek TH, Gothic, Slate, or Snodgrass. For 18-19 there were no changes to Brush Creek TH or Slate. 
@@ -77,6 +77,7 @@ trailheads1920 <- trailheads1920 %>% rename(date = Date)
 # Add. changes: change spaces to underscores, date to date_time
 
 caic17 <- read_csv("Data_for_Zack/CAIC_Data_2017-2020/caic2017.csv")
+caic17 <- caic17[-1,] # No other data for 2017-12-21
 caic18 <- read_csv("Data_for_Zack/CAIC_Data_2017-2020/caic2018.csv")
 caic19 <- read_csv("Data_for_Zack/CAIC_Data_2017-2020/caic2019.csv")
 
@@ -152,36 +153,6 @@ winter1718 <- list(trailheads1718, caic17, snotel17) %>% reduce(full_join, by="d
 winter1819 <- list(trailheads1819, caic18, snotel18) %>% reduce(full_join, by="date")
 winter1920 <- list(trailheads1920, caic19, snotel19) %>% reduce(full_join, by="date")
 
-## Pivot to long format
-
-winter1718 <- pivot_longer(winter1718, 
-                           c("Non.motorized", "Mechanized", "Motorized", "Hybrid"), 
-                           names_to = "modality", values_to = "user.count")
-winter1718$modality <- factor(winter1819$modality)
-
-winter1819 <- pivot_longer(winter1819, 
-                           c("Non.motorized", "Mechanized", "Motorized", "Hybrid"), 
-                           names_to = "modality", values_to = "user.count")
-winter1819$modality <- factor(winter1819$modality)
-
-winter1920 <- pivot_longer(winter1920, 
-                           c("Non.motorized", "Mechanized", "Motorized", "Hybrid"), 
-                           names_to = "modality", values_to = "user.count")
-winter1920$modality <- factor(winter1920$modality)
-
-## Add some variables
-
-winter1718$week <- factor(wday(winter1718$date), labels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
-winter1718$weekend <- ifelse(winter1718$week %in% c("Sat", "Sun"), "weekend", "weekday")
-winter1819$week <- factor(wday(winter1819$date), labels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
-winter1819$weekend <- ifelse(winter1819$week %in% c("Sat", "Sun"), "weekend", "weekday")
-winter1920$week <- factor(wday(winter1920$date), labels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
-winter1920$weekend <- ifelse(winter1920$week %in% c("Sat", "Sun"), "weekend", "weekday")
-
-## Summaries to check reasonableness
-
-summary(winter1819)
-
 ## Join all years
 
 winter1718$year <- "w1718"
@@ -190,6 +161,29 @@ winter1920$year <- "w1920"
 
 winter_travel <- bind_rows(list(winter1718,winter1819,winter1920))
 
+## Fix some capitalization issues 
+
+winter_travel <- winter_travel %>% mutate(Non.motorized = coalesce(Non.Motorized, Non.motorized))
+winter_travel$Non.Motorized <- NULL
+
+winter_travel[winter_travel$Trailhead=="Slate River","Trailhead"] <- "Slate River Rd"
+
+## Pivot to long format
+
+winter_travel <- pivot_longer(winter_travel, 
+                           c("Non.motorized", "Mechanized", "Motorized", "Hybrid"), 
+                           names_to = "modality", values_to = "user.count")
+winter_travel$modality <- factor(winter_travel$modality)
+
+
+## Add some variables
+
+winter_travel$week <- factor(wday(winter_travel$date), labels = c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))
+winter_travel$weekend <- ifelse(winter_travel$week %in% c("Sat", "Sun"), "weekend", "weekday")
+
+## Summaries to check reasonableness
+
+summary(winter_travel)
 
 #  1. How do trail visitor counts correlate to an increase in snowfall during the previous day(s)? 
 #     (what patterns of snowfall yield the most visitors/changes in visitation)
@@ -198,78 +192,103 @@ ggplot(winter_travel, aes(change_depth, user.count, color = Trailhead))+
   geom_jitter(height = 0, width = 0.2, alpha = 0.5)+
   facet_grid(modality~year)
 
-ggplot(winter1819, aes(change_depth, user.count, color = rating_near))+
+ggplot(winter_travel, aes(change_depth, user.count, color = rating_near))+
   geom_jitter(height = 0, width = 0.2, alpha = 0.7)+
   scale_color_manual(values = c("green", "yellow", "orange", "red"))+
   scale_y_log10()+
   facet_grid(modality~Trailhead)
 
-ggplot(winter1819, aes(past3snow, user.count, color = rating_near))+
+ggplot(winter_travel, aes(past3snow, user.count, color = rating_near))+
   geom_jitter(height = 0, width = 0.2, alpha = 0.7)+
   scale_color_manual(values = c("green", "yellow", "orange", "red"))+
   scale_y_log10()+
   facet_grid(modality~Trailhead)
 
-ggplot(winter1819, aes(past5snow, user.count, color = rating_near))+
+ggplot(winter_travel, aes(past5snow, user.count, color = rating_near))+
   geom_jitter(height = 0, width = 0.2, alpha = 0.7)+
   scale_color_manual(values = c("green", "yellow", "orange", "red"))+
   scale_y_log10()+
   facet_grid(modality~Trailhead)
 
-glm1 <- glm(user.count ~ modality*Trailhead+change_depth+past2snow+past3snow+past5snow, family = poisson, data=winter1819)
+glm1 <- glm(user.count ~ modality*Trailhead+change_depth+past2snow+past3snow+past5snow, family = poisson, data=winter_travel)
 Anova(glm1, test.statistic = "LR")
 summary(glm1)
-confint(glm1)
-stepAIC(glm1)
+hist(resid(glm1))
+
+coef(summary(glm1))[c(12, 15),]
+confint(glm1)[c(12, 15),]
+# Looks like about a 1%-2% decrease in users per cm of new snow, and an additional 0.4%-0.9% decrease per cm of new snow in the past 5 days.
+
 
 #  2. How does an increased avalanche risk impact the numbers of hybrid and motorized visitors to specific trailheads?
 
-ggplot(na.omit(winter1819), aes(rating_near, user.count, color = Trailhead, fill = Trailhead))+
+ggplot(na.omit(winter_travel), aes(rating_near, user.count, color = Trailhead, fill = Trailhead))+
   geom_point(position=position_jitterdodge(jitter.width = 0.2))+
   geom_boxplot(color="black", outlier.shape = NA)+
   scale_y_log10()+
   facet_wrap(vars(modality))
 
 
-glm2 <- glm(user.count ~ modality*Trailhead*(rating_below+rating_near+rating_above), family = poisson, data=winter1819)
+glm2 <- glm(user.count ~ modality*Trailhead*rating_near, family = poisson, data=winter_travel)
 Anova(glm2, test.statistic = "LR")
 summary(glm2)
-stepAIC(glm2)
+
 
 #  3. What combination of weather conditions yields the highest/lowest number of visitors?
 
-ggplot(winter1819, aes(air_temp, past5snow, size = user.count, color = rating_near))+
+ggplot(winter_travel, aes(air_temp, past5snow, size = user.count, color = rating_near))+
   geom_jitter(height = 0.2, width = 0.2, alpha = 0.7)+
   scale_color_manual(values = c("green", "yellow", "orange", "red"))
 
-ggplot(winter1819, aes(air_temp, past5snow, size = user.count, color = rating_near))+
+ggplot(winter_travel, aes(air_temp, change_depth, size = user.count, color = rating_near))+
+  geom_jitter(height = 0.2, width = 0.2, alpha = 0.7)+
+  scale_color_manual(values = c("green", "yellow", "orange", "red"))
+
+ggplot(winter_travel, aes(air_temp, past5snow, size = user.count, color = rating_near))+
   geom_jitter(height = 0, width = 0.2, alpha = 0.7)+
   scale_color_manual(values = c("green", "yellow", "orange", "red"))+
   facet_grid(modality~Trailhead)
 
-glm3 <- glm(user.count~air_temp+change_depth+snow_depth, family = poisson, data=winter1819)
+glm3 <- glm(user.count~air_temp+change_depth+snow_depth, family = poisson, data=winter_travel)
 summary(glm3)
 
+# Looks like with new snow there are fewer users. About 1.8% per cm of new snow. Warmer air temps also bring people out, about 1% per degree.
+
+
 #  4. What sample size is needed to represent the entire season ?
+
+
+
 #  5. Which days of the week have the highest number of visitors? Weekdays vs weekends?
 
-glm5 <- glm(user.count~weekend, family = poisson, data=winter1819)
+glm5 <- glm(user.count~weekend, family = poisson, data=winter_travel)
 summary(glm5)
 
+glm5a <- glm(user.count~week, family = poisson, data=winter_travel)
+summary(glm5a)
+
+# Saturdays are about 12.5% busier than Sundays, weekdays are about half as busy as weekends.
+
 #  6. How do visitation rates for different user groups vary seasonally?
+
+ggplot(winter_travel, aes(year, user.count))+
+  geom_jitter(height = 0, width = 0.2, alpha = 0.7)+
+  facet_grid(modality~Trailhead)+
+  scale_y_log10()
+
 
 
 #  7. Which days of the year are busiest?
 
-day_totals1819 <- winter1819 %>% 
+day_totals <- winter_travel %>% 
   group_by(date) %>%
   summarise(users = sum(user.count, na.rm = TRUE), 
             week = unique(week)) %>%
   arrange(desc(users))
 
-head(day_totals1819, 10)
+head(day_totals, 10)
 
-ggplot(day_totals1819, aes(date, users))+
+ggplot(day_totals, aes(date, users))+
   geom_line()+
   geom_point(aes(color=week))
 
@@ -277,24 +296,24 @@ ggplot(day_totals1819, aes(date, users))+
 #  8. Do different groups of users utilize the trail more during different times of the season?
 
 
-ggplot(winter1819, aes(date, user.count+1, color = Trailhead))+
+ggplot(winter_travel, aes(date, user.count+1, color = Trailhead))+
   geom_point()+
   geom_smooth()+
   scale_y_log10()+
   facet_wrap(vars(modality))
 
-gam6 <- gam(user.count ~ modality*s(date), family = poisson, data=winter1819)
+gam6 <- gam(user.count ~ modality*s(date), family = poisson, data=winter_travel)
 summary(gam6)
 
-modality_day_totals1819 <- winter1819 %>% 
+modality_day_totals <- winter_travel %>% 
   group_by(date, modality) %>%
   summarise(users = sum(user.count, na.rm = TRUE), 
             week = unique(week)) %>%
   arrange(desc(users))
 
-head(modality_day_totals1819, 10)
+head(modality_day_totals, 10)
 
-ggplot(modality_day_totals1819, aes(date, users, color = modality))+
+ggplot(modality_day_totals, aes(date, users, color = modality))+
   geom_line()+
   geom_smooth()+
   scale_y_log10()
@@ -304,5 +323,5 @@ ggplot(modality_day_totals1819, aes(date, users, color = modality))+
 #  10. What are major year-to-year trends in visitation?
 #  11. How do the days with the highest avalanche risk affect visitor rates?
 
-high_risk <- as.numeric(winter1819$rating_above)+as.numeric(winter1819$rating_near)+as.numeric(winter1819$rating_below) > 9
-summary(glm(user.count~high_risk, data=winter1819, family = poisson))
+high_risk <- as.numeric(winter_travel$rating_above)+as.numeric(winter_travel$rating_near)+as.numeric(winter_travel$rating_below) > 9
+summary(glm(user.count~high_risk, data=winter_travel, family = poisson))
